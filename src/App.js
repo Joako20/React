7898649972e08907da74d0ea5e47677b82ed19d7
Joako20 <leo_j_carrizo@hotmail.com>
2021-03-29 10:20:56 -0300

    navbar agregado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,6 +5,7 @@ function App() {
     <div className="App">
       <h1>E-comerce Horizon</h1>
     </div>
+    
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+function NavBar() {
+    return <>
+    <nav class="sections">
+        <ul class="links-nav">
+            <li><a href="./secciones/ofertas.html">Ofertas</a></li>
+            <li><a href="./secciones/productos.html">Productos</a></li>
+            <li><a href="./secciones/sobre-nosotros.html">Sobre Nosotros</a></li>
+            <li><a href="./secciones/contactanos.html">Contactanos</a></li>
+        </ul>
+    </nav>
+</>
+}
+
+export default NavBar;
